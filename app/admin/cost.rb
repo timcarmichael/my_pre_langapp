2bb3bb1564e79083b4fb3f2cd05378cf314b0101
Prelang Builder <builder@prelang.com>
2016-09-17 20:03:06 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Cost' to allow for create, update

--- app/admin/cost.rb
+++ app/admin/cost.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Cost do
+  permit_params :description, :cost, :supplier
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
